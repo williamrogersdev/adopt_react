4a8b086ea4e3c6b4b3fffd5e1bafa36494d5aef1
William Rogers <williamrogersdev@gmail.com>
2022-09-26 00:23:13 -0500

    update

---
+++ src/Pet.js
@@ -0,0 +1,21 @@
+// import React from "react";
+
+const Pet = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h1>{props.breed}</h1>
+    </div>
+  );
+};
+
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+// };
+
+export default Pet;
